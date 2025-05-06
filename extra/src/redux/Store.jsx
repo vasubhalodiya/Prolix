658a53c5d79c0d9f7801607b0c468e5b92975ba0
Vasu Bhalodiya <vasubhalodiya30@gmail.com>
2025-05-06 15:15:46 +0530

    update

--- extra/src/redux/Store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { userApi } from './userApi';
-
-export const store = configureStore({
-  reducer: {
-    [userApi.reducerPath]: userApi.reducer,
-  },
-  middleware: (getDefault) => getDefault().concat(userApi.middleware),
-});
